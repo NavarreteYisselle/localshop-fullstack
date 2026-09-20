58e5149c6c9604cefdc4eb2cb39213116882f90d
NavarreteYisselle <yiselle.navarrete21@gmail.com>
2026-09-20 01:15:50 +0000

    Install Bootstrap and create LocalShop homepage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,19 @@
 function App() {
   return (
-    <div>
-      <h1>LocalShop</h1>
-      <p>Tu tienda online, facil y segura.</p>
+    <div className="container py-5">
+      <h1 className="text-center mb-4">
+        Bienvenido a LocalShop
+      </h1>
+
+    <p className="text-center text-muted">
+      Tu tienda online, facil y segura.
+    </p>
+
+    <div className="text-center mt-4">
+      <button className="btn btn-primary">
+          Explorar productos
+      </button>
+    </div>
     </div>
   );
 }
